1730fafb29b191ab8fd8c3e6151cd2af2347d408
Artem Golovchenko <golovchenko84@mail.ru>
2017-02-10 15:21:03 +0200

    change aop configure

--- src/main/java/com/springinaction/springidol/Audience.java
+++ src/main/java/com/springinaction/springidol/Audience.java
@@ -1,5 +1,7 @@
 package com.springinaction.springidol;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+
 /**
  * Created by головченко on 10.02.2017.
  */
@@ -18,5 +20,21 @@ public class Audience {
 
     public void demandRefund() { //<co id="co_demandRefund"/>
         System.out.println("Boo! We want our money back!");
+    }
+
+
+    public void watchPerformance(ProceedingJoinPoint joinpoint) {
+        try {
+            System.out.println("The audience is taking their seats.");
+            System.out.println("The audience is turning off their cellphones");
+            long start = System.currentTimeMillis(); // Перед выступлением
+            joinpoint.proceed(); // Вызов целевого метода
+            long end = System.currentTimeMillis(); // После выступления
+            System.out.println("CLAP CLAP CLAP CLAP CLAP");
+            System.out.println("The performance took " + (end - start)
+                    + " milliseconds.");
+        } catch (Throwable t) {
+            System.out.println("Boo! We want our money back!");
+        }
     }
 }
